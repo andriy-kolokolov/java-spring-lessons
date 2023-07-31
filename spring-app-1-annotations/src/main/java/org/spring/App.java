f5a84d8e3bf682fa281b46d041c1a1f8657ada0b
Andriy-Kolokolov <koloandre22@gmail.com>
2023-07-31 16:00:27 +0200

    add annotation spring config

--- spring-app-1-annotations/src/main/java/org/spring/App.java
+++ spring-app-1-annotations/src/main/java/org/spring/App.java
@@ -1,11 +1,12 @@
 package org.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
 
 //        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
